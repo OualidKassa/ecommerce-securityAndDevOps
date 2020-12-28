49d9ecf2b2437a68ace97b3556b798e952fa1aad
OualidKassa <Shoananas94@>
2020-12-28 15:57:12 +0100

    first commit

---
+++ src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -0,0 +1,18 @@
+package com.example.demo.model.requests;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CreateUserRequest {
+
+
+	@JsonProperty
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
